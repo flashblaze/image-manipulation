4484bbaac9952ed99478cec3b352d26e9c774816
FlashBlaze <neerajlagwankar@gmail.com>
2020-03-18 08:24:51 +0530

    This should pass the test

--- __tests__/testing-library.js
+++ __tests__/testing-library.js
@@ -4,6 +4,6 @@ import Index from '../pages/index';
 
 test('renders deploy link', () => {
   const { getByText } = render(<Index />);
-  const linkElement = getByText(/Upload a file/);
-  expect(linkElement).toBeInTheDocument();
+  const titleText = getByText(/Image manipulator/);
+  expect(titleText).toBeInTheDocument();
 });
